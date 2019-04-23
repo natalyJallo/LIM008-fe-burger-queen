cfe92d6e0b77ec79317612f8537331a70a7ce572
natt17 <marketing20.nat@gmail.com>
2019-04-23 13:18:34 -0500

    se añadio algunas propiedades de diseño con css

--- burger-queen/src/environments/environment.prod.ts
+++ burger-queen/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDUu8dkAHmEG5LChidsoO6wkHVCRy44RCM",
+    authDomain: "burger-queen-8fb20.firebaseapp.com",
+    databaseURL: "https://burger-queen-8fb20.firebaseio.com",
+    projectId: "burger-queen-8fb20",
+    storageBucket: "burger-queen-8fb20.appspot.com",
+    messagingSenderId: "939389989877"
+  }
 };
